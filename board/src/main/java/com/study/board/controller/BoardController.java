4131e089aa34bc461c7d3c06dccdd9b1e13e8d7e
choe9 <run1529@gmail.com>
2023-11-27 17:13:39 +0900

    first commit

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/write") //localhost:8888/board/write
+    public String boardWriteForm() {
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content) {
+
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+
+        return "";
+    }
+}
+
